ae2c1c96d603ec0fa2e88354bd04112bef09cc63
ShamaunStrickland <shamaunstrickland@gmail.com>
2024-03-25 01:03:59 -0400

    Major Server Update

--- Anadros/TerminalInterface.py
+++ Anadros/TerminalInterface.py
@@ -1,41 +1,26 @@
-import os
+import re
 
 
 class TerminalInterface:
-    class Color:
-        PINK = '\033[95m'
-        BLUE = '\033[94m'
-        CYAN = '\033[96m'
-        GREEN = '\033[92m'
-        GOLDEN = '\033[93m'
-        RED = '\033[91m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
-        END = '\033[0m'
-
     @staticmethod
-    def get_user_input(name="User", name_colour=Color.PINK, prompt_symbol="➤"):
+    def get_user_input(prompt_symbol="➤"):
         """Prompt the user for input."""
-        # Set the TERM environment variable
-        os.environ['TERM'] = 'xterm-256color'
-
-        formatted_name = f"{TerminalInterface.Color.BOLD}{name}{TerminalInterface.Color.END}"
-        prompt = f"{name_colour}{formatted_name}{TerminalInterface.Color.END} {prompt_symbol} "
-        user_input = input(prompt)
-        return user_input
+        return input(f"{prompt_symbol} ")
 
     @staticmethod
-    def bot_response(text, name="Bot", name_colour=Color.GREEN, text_colour=Color.END):
+    def bot_response(text):
         """Display the bot's response."""
-        formatted_name = f"{TerminalInterface.Color.BOLD}{name}{TerminalInterface.Color.END}"
-        print(
-            f"{name_colour}{formatted_name}{TerminalInterface.Color.END}: {text_colour}{text}{TerminalInterface.Color.END}")
+        print(text)
 
     @staticmethod
     def clean_up(text):
-        """Clean up the text by removing non-user and non-bot information."""
+        """Clean up the text by removing ANSI escape codes and non-user/bot information."""
+        # Remove ANSI escape codes
+        text_no_ansi = re.sub(r'\x1b\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]', '', text)
+        # Remove specific sequence
+        text_no_ansi = text_no_ansi.replace('\x1B[3J\x1B[H\x1B[2J', '')
         cleaned_text = ""
-        for line in text.split("\n"):
+        for line in text_no_ansi.split("\n"):
             if line.startswith("User:") or line.startswith("Bot:"):
                 cleaned_text += line + "\n"
         return cleaned_text

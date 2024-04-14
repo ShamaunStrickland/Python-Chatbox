8a0f6d12082e36500e5da01a2fa2e240bf5969e9
ShamaunStrickland <shamaunstrickland@gmail.com>
2024-04-14 16:34:30 -0400

    Fixed Js to handle Https requests

--- Anadros/apps.py
+++ Anadros/apps.py
@@ -2,9 +2,15 @@ import subprocess
 import re
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import ssl
 
 app = Flask(__name__, template_folder='AnadrosSite', static_folder='static')
-socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins=["https://anadros.com"])
+
+# Create SSL context
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain('/etc/nginx/ssl/ssl_certificate.pem', '/etc/nginx/ssl/ssl_certificate_key.pem')
+
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins=["https://anadros.com"], ssl_context=ssl_context)
 
 
 # Function to log IP addresses and chat messages

8756cc82033cb9fb8d2d42ed424b5e050628bcdd
ShamaunStrickland <shamaunstrickland@gmail.com>
2024-10-05 21:32:23 -0400

    Websocket testing

--- Anadros/static/script.js
+++ Anadros/static/script.js
@@ -1,18 +1,29 @@
 import {io} from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 
-const socket = io('wss://anadros.com', {
+const socket = io('http://anadros.com', {  // Changed 'wss' to 'http'
     transports: ['websocket'],
     reconnectionAttempts: 5,
 });
 
+// Connection success handler
 socket.on('connect', () => {
     console.log('Successfully connected to the server.');
+});
+
+// Error handling for connection issues
+socket.on('connect_error', (error) => {
+    console.error('Connection Error:', error);
 });
 
 // Event listener for 'bot_response' event
 socket.on('bot_response', (data) => {
     console.log('Received response:', data);
     displayBotResponse(data);
+});
+
+// Handle disconnection
+socket.on('disconnect', (reason) => {
+    console.log('Disconnected:', reason);
 });
 
 function displayBotResponse(response) {
